5cc83c742d73a1aa6b8df67a68ddb938fc963b07
Qasimali20 <124170100+Qasimali20@users.noreply.github.com>
2023-08-04 23:06:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,74 +1,46 @@
 import streamlit as st
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 
-# Load the Spambase dataset
-url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data'
-column_names = [
-    'word_freq_make', 'word_freq_address', 'word_freq_all', 'word_freq_3d',
-    'word_freq_our', 'word_freq_over', 'word_freq_remove', 'word_freq_internet',
-    'word_freq_order', 'word_freq_mail', 'word_freq_receive', 'word_freq_will',
-    'word_freq_people', 'word_freq_report', 'word_freq_addresses', 'word_freq_free',
-    'word_freq_business', 'word_freq_email', 'word_freq_you', 'word_freq_credit',
-    'word_freq_your', 'word_freq_font', 'word_freq_000', 'word_freq_money',
-    'word_freq_hp', 'word_freq_hpl', 'word_freq_george', 'word_freq_650',
-    'word_freq_lab', 'word_freq_labs', 'word_freq_telnet', 'word_freq_857',
-    'word_freq_data', 'word_freq_415', 'word_freq_85', 'word_freq_technology',
-    'word_freq_1999', 'word_freq_parts', 'word_freq_pm', 'word_freq_direct',
-    'word_freq_cs', 'word_freq_meeting', 'word_freq_original', 'word_freq_project',
-    'word_freq_re', 'word_freq_edu', 'word_freq_table', 'word_freq_conference',
-    'char_freq_;', 'char_freq_(', 'char_freq_[', 'char_freq_!', 'char_freq_$',
-    'char_freq_#', 'capital_run_length_average', 'capital_run_length_longest',
-    'capital_run_length_total', 'label'
+# Sample email data and labels
+sample_data = [
+    ("Get rich quick! Earn $1000s with our amazing offer.", 1),  # Spam
+    ("Hi there, just checking in. How are you?", 0),  # Not spam
+    ("URGENT: Your account needs verification.", 1),  # Spam
+    ("Let's meet for coffee this weekend.", 0),  # Not spam
 ]
-data = pd.read_csv(url, header=None, names=column_names)
-X = data.drop('label', axis=1)
-y = data['label']
+
+# Create a DataFrame from the sample data
+data = pd.DataFrame(sample_data, columns=['email', 'label'])
 
 # Split the data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(data['email'], data['label'], test_size=0.2, random_state=42)
+
+# Initialize the TfidfVectorizer
+vectorizer = TfidfVectorizer()
+
+# Fit and transform the vectorizer on the training data
+X_train_vectorized = vectorizer.fit_transform(X_train)
 
 # Train the classifier
 classifier = MultinomialNB()
-classifier.fit(X_train, y_train)
+classifier.fit(X_train_vectorized, y_train)
 
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-
+# Streamlit app
 def main():
-    st.title("Spam Email Detection")
-    email_input = st.text_area("Enter numerical attributes for prediction (comma-separated):")
-    
+    st.title("Custom Spam Email Detection")
+    email_input = st.text_area("Enter an email:")
+
     if st.button("Predict"):
-        try:
-            attributes = [float(attr) for attr in email_input.split(',') if attr.strip()]
-            prediction = classifier.predict([attributes])[0]
-            
-            if prediction == 0:
-                st.write("This email is likely not spam.")
-            else:
-                st.write("This email is likely spam.")
-        except ValueError:
-            st.write("Invalid input. Please enter valid numerical attributes separated by commas.")
+        vectorized_email = vectorizer.transform([email_input])
+        prediction = classifier.predict(vectorized_email)[0]
 
-
-
-
-
-
+        if prediction == 0:
+            st.write("This email is likely not spam.")
+        else:
+            st.write("This email is likely spam.")
 
 if __name__ == "__main__":
     main()
-    
