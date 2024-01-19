6595103dab44b4a1b9c846ac33e89429671fc704
Qasimali20 <u2022414@giki.edu.pk>
2024-01-19 22:33:55 +0500

    del

--- HomeMaking/helloworldapp/forms.py
+++
@@ -1,5 +0,0 @@
-# calculator/forms.py
-from django import forms
-
-class CalculatorForm(forms.Form):
-    expression = forms.CharField(label='Enter an expression', max_length=200)
--- HomeMaking/helloworldapp/utils.py
+++
@@ -1,4 +0,0 @@
-def read_emails(start, end):
-    with open('C:/Users/user/OneDrive/Desktop/osama/project/data/MailPassPirate_5198.txt', 'r', encoding='utf-8') as file:
-        emails = file.readlines()
-    return emails[start:end]
